aef831dd1b6ae0e080f96845b47537eefba6379d
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2025-02-04 08:39:25 +0100

    first commit

---
+++ myapp/resources/js/main.js
@@ -0,0 +1,17 @@
+let getUsername = async () => {
+    const key = NL_OS == 'Windows' ? 'USERNAME' : 'USER';
+    let value = '';
+    try {
+        value = await Neutralino.os.getEnv(key);
+    }
+    catch(err) {
+        console.error(err);
+    }
+    document.getElementById('name').innerText = `Hello ${value}`;
+  }
+  
+  Neutralino.init();
+  Neutralino.events.on("windowClose", () => {
+    Neutralino.app.exit();
+  });
+  getUsername();
