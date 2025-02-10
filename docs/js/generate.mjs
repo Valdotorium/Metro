4baf1a625db962ce8ec83798dd390b9934b06f1c
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2025-02-10 17:55:24 +0100

    random map

--- docs/js/generate.mjs
+++ docs/js/generate.mjs
@@ -1,13 +1,28 @@
+function generateTilemapArray(mapSize){
+    //generate a tilemap array with widt mapSize and height mapSize
+    const tilemapArray = Array.from({length: mapSize}, () => Array.from({length: mapSize}, () => 0));
+
+    //fill the array with random numbers between 0 and 15
+    for(let i = 0; i < mapSize; i++){
+        for(let j = 0; j < mapSize; j++){
+            tilemapArray[i][j] = Math.floor(Math.random() * 15);
+        }
+    }
+    return tilemapArray;
+}
+
+
 export function generateTilemap(game){
     //get the tilemap size
     const mapSize = game.tileMapOptions.get("size")
 
     console.log("map size is: ", mapSize)
-
-    game.tileMap = game.make.tilemap({ tileWidth: 8, tileHeight: 8, width: mapSize, height: mapSize})
+    //generate a tilemap array
+    game.generatedTilemap = generateTilemapArray(mapSize)
+    
+    game.tileMap = game.make.tilemap({ data: game.generatedTilemap, tileWidth: 8, tileHeight: 8, width: mapSize, height: mapSize})
     const tileset = game.tileMap.addTilesetImage('tileset');
-    const layer = game.tileMap.createBlankLayer('layer1', tileset,0,0);
-    layer.fill(1,0,0,mapSize,mapSize)
+    const layer = game.tileMap.createLayer(0, tileset,0,0);
     layer.setScale(4);
 
     const cursors = game.input.keyboard.createCursorKeys();
