f2016134ab0044544f802babab4b952243d57a58
JoEragon <mail@johannesfeldmann.de>
2025-02-14 12:53:24 +0100

    working camera controll

--- docs/js/input/mouse.mjs
+++ docs/js/input/mouse.mjs
@@ -3,35 +3,27 @@ export function setupMouse(game){
     game.mouse = game.input.activePointer
 }
 let counter = 0;
+let cameraDragStartX;
+let cameraDragStartY;
 export function dragCamera(game){
 
     //update camera position based on mouse position if clicked och valle das wollte ich machen
     //TODO: #2 issue with window resizability and mouse position, need to keep height widt ratio constant?
     const camera = game.cameras.main;
-    let cameraDragStartX;
-    let cameraDragStartY;
     let pointer = game.input.activePointer;
-    
-    if (game.input.activePointer.isDown&&counter==0){
-        console.log(pointer.y,pointer.x)
-        
-        counter++
+
+    if (game.input.activePointer.isDown&&/*game.keys.get("SHIFT").isDown&&*/counter==1) {
+        camera.scrollX = camera.scrollX + ((cameraDragStartX - pointer.x)/camera.zoom*1.2)
+        camera.scrollY = camera.scrollY + ((cameraDragStartY - pointer.y)/camera.zoom*1.2)
     }
-    game.input.on(
-        "pointerup",
-        function (pointer) {
-          counter = 0;
-        })
-
-    if (game.input.activePointer.isDown&&game.keys.get("SHIFT").isDown) {
-
-        camera.scrollX = pointer.x + cameraDragStartX;
-        camera.scrollY = pointer.y + cameraDragStartY;
-        console.log("aaaaaaaaaaaaaaaaa")
-    };
-
-
-
+    if (game.input.activePointer.isDown&&counter==0/*&&game.keys.get("SHIFT").isDown*/){
+        cameraDragStartX = pointer.x
+        cameraDragStartY = pointer.y
+        counter++
+    }else{
+        counter = 0
+    }
+    
 
 }
 
