85f980ab6df8f70399b761ca98841053111a940e
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2025-02-18 07:10:04 +0100

    tweaked some value

--- docs/js/generator/complexTerrain.mjs
+++ docs/js/generator/complexTerrain.mjs
@@ -14,14 +14,14 @@ export function generateComplexTilemap(size){
     //noise for specific tile types
     let forestNoise = generatePerlinNoise(size, 5)
     let sandNoise = generatePerlinNoise(size, 10)
-    let mountainNoise = generatePerlinNoise(size, 3)
-    let mountainBaseNoise = generatePerlinNoise(size, 20)
+    let mountainBaseNoise = generatePerlinNoise(size, 3)
+    let mountainNoise = generatePerlinNoise(size, 20)
 
     //overlay the mountain noises
     for(let i = 0; i < size; i++){
         for(let j = 0; j < size; j++){
             if(mountainNoise[i][j] > 0.8){
-                mountainNoise[i][j] = mountainBaseNoise[i][j] * 0.75 + mountainNoise[i][j] * 0.25
+                mountainNoise[i][j] = mountainBaseNoise[i][j] * 0.4 + mountainNoise[i][j] * 0.6
             }
         }
     }
@@ -31,7 +31,7 @@ export function generateComplexTilemap(size){
     for(let i = 0; i < size; i++){
         weightedNoise[i] = [];
         for(let j = 0; j < size; j++){
-            let noiseValue = (baseNoise[i][j] * 0.55  + complexNoise[i][j] * 0.375 + shuffleNoise[i][j] * 0.075)
+            let noiseValue = (baseNoise[i][j] * 0.54  + complexNoise[i][j] * 0.39 + shuffleNoise[i][j] * 0.07)
             weightedNoise[i][j] = noiseValue;
         } 
     }
@@ -39,25 +39,25 @@ export function generateComplexTilemap(size){
     //translate the values between 0 and 1 to tile types. see tilemapIndexxes.txt for more info
     for(let i = 0; i < size; i++){
         for(let j = 0; j < size; j++){
-            if(weightedNoise[i][j] < 0.4){
+            if(weightedNoise[i][j] < 0.42){
                 //water
                 weightedNoise[i][j] = 22
-            } else if(weightedNoise[i][j] < 0.48 && sandNoise[i][j] > 0.6){
+            } else if(weightedNoise[i][j] < 0.48 && sandNoise[i][j] > 0.55){
                 //beach
                 weightedNoise[i][j] = 2
-            }else if(weightedNoise[i][j] < 0.48 && forestNoise[i][j] > 0.7){
+            }else if(weightedNoise[i][j] < 0.48 && forestNoise[i][j] > 0.75){
                 //swamp
                 weightedNoise[i][j] = 7
-            }else if(weightedNoise[i][j] < 0.44 && sandNoise[i][j] < 0.5 && forestNoise[i][j] < 0.6){
+            }else if(weightedNoise[i][j] < 0.44 && sandNoise[i][j] < 0.45 && forestNoise[i][j] < 0.5){
                 //wetlands
                 weightedNoise[i][j] = 8
-            }else if(weightedNoise[i][j] < 0.55 && forestNoise[i][j] > 0.7){
+            }else if(weightedNoise[i][j] < 0.55 && forestNoise[i][j] > 0.6){
                 //forest
                 weightedNoise[i][j] = 6
-            }else if(weightedNoise[i][j] < 0.725 && forestNoise[i][j] > 0.8){
+            }else if(weightedNoise[i][j] < 0.725 && forestNoise[i][j] > 0.7){
                 //forest
                 weightedNoise[i][j] = 6
-            }else if(weightedNoise[i][j] < 0.65 && forestNoise[i][j] < 0.2){
+            }else if(weightedNoise[i][j] < 0.65 && forestNoise[i][j] < 0.25){
                 //field
                 weightedNoise[i][j] = 1
             }else if(weightedNoise[i][j] < 0.725 && forestNoise[i][j] < 0.1){
@@ -66,13 +66,13 @@ export function generateComplexTilemap(size){
             }else if(weightedNoise[i][j] < 0.725){
                 //plains
                 weightedNoise[i][j] = 0
-            } else if(weightedNoise[i][j] < 0.78){
+            } else if(weightedNoise[i][j] < 0.76){
                 //forest (lower)
                 weightedNoise[i][j] = 6
-            } else if(weightedNoise[i][j] > 0.86 && mountainNoise[i][j] > 0.55){
+            } else if(weightedNoise[i][j] > 0.85 && mountainNoise[i][j] > 0.525){
                 //mountains snowcaps
                 weightedNoise[i][j] = 3
-            } else if(mountainNoise[i][j] > 0.35){
+            } else if(mountainNoise[i][j] > 0.375){
                 //mountains
                 weightedNoise[i][j] = 4
             }  else {
