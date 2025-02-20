e7a74a2e88aef7dba5eb67a6a644c7cda0e4df79
JoEragon <116171114+JoEragon@users.noreply.github.com>
2025-02-20 12:03:29 +0100

    Fixed freezing on negative x and y koordinates

--- docs/js/input/selectTiles.mjs
+++ docs/js/input/selectTiles.mjs
@@ -14,17 +14,13 @@ export function setupCurrentTileMarker(game){
 export function getHoveredTile(game){
 
     // Rounds down to nearest tile
-    //if(game.mouse.worldPoint.x < 0){game.mouse.worldPoint.x = 0} 
-    //if(game.mouse.worldPoint.y < 0){game.mouse.worldPoint.y = 0}
+    if(game.mouse.worldPoint.x < 0){game.mouse.worldPoint.x = 0} 
+    if(game.mouse.worldPoint.y < 0){game.mouse.worldPoint.y = 0}
     //if(game.mouse.worldPoint.x > game.tileMapOptions.get("size")){game.mouse.worldPoint.x = game.tileMapOptions.get("size")}
     //if(game.mouse.worldPoint.y > game.tileMapOptions.get("size")){game.mouse.worldPoint.y = game.tileMapOptions.get("size")}
     const pointerTileX = game.tileMap.worldToTileX(game.mouse.worldPoint.x);
     const pointerTileY = game.tileMap.worldToTileY(game.mouse.worldPoint.y);
     //store the tile that is currently hovered
-    if(pointerTileX < 0){pointerTileX = 0}
-    if(pointerTileY < 0){pointerTileY = 0}
-    if(pointerTileX > game.tileMapOptions.get("size")){pointerTileX = game.tileMapOptions.get("size")}
-    if(pointerTileY > game.tileMapOptions.get("size")){pointerTileY = game.tileMapOptions.get("size")}
     
     game.currentHoveredTile = game.tileMap.getTileAt(pointerTileX, pointerTileY);
     // Snap to tile coordinates, but in world space
