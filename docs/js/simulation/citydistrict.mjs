16d7cc88dcd46569bd72209c22e91a574a238d25
JoEragon <mail@johannesfeldmann.de>
2025-02-24 22:01:48 +0100

    ölkjhgfdsdfghjklöä i am going insane

--- docs/js/simulation/citydistrict.mjs
+++ docs/js/simulation/citydistrict.mjs
@@ -4,27 +4,28 @@ export class cityDistrict {
     constructor(id, city, posx, posy, game) {
         this.id = id;
         this.validNeighbours = [];
-        this.city = city;
         this.posx = posx;
         this.posy = posy;
-        this.addNeighbourTiles(game, city, posx, posy);
-        game.generatedTilemap[posx][posy] = 10;
+        this.addNeighbourTiles(game, posx, posy);
+        game.generatedTilemap[posx][posy] = 2;
     }
 
-    addNeighbourTiles(game, posx, posy) {
-        if (0 < game.generatedTilemap[(posx + 1)][(posy)] && game.generatedTilemap[(posx + 1)][posy] < 4) {
+    addNeighbourTiles(game) {
+        let posx = 7;
+        let posy = 7;
+        if (0 <= game.generatedTilemap[posx+1][posy] && game.generatedTilemap[posx + 1][posy+0] < 4) {
             let neighbour = [posx + 1, posy];
             this.validNeighbours.push(neighbour);
         }
-        if (0 < game.generatedTilemap[posx][posy - 1] && game.generatedTilemap[posx][posy - 1] < 4) {
+        if (0 <= game.generatedTilemap[posx][posy - 1] && game.generatedTilemap[posx][posy - 1] < 4) {
             let neighbour = [posx, posy - 1];
             this.validNeighbours.push(neighbour);
         }
-        if (0 < game.generatedTilemap[posx - 1][posy] && game.generatedTilemap[posx - 1][posy] < 4) {
+        if (0 <= game.generatedTilemap[posx - 1][posy] && game.generatedTilemap[posx - 1][posy] < 4) {
             let neighbour = [posx - 1, posy];
             this.validNeighbours.push(neighbour);
         }
-        if (0 < game.generatedTilemap[posx][posy + 1] && game.generatedTilemap[posx][posy + 1] < 4) {
+        if (0 <= game.generatedTilemap[posx][posy + 1] && game.generatedTilemap[posx][posy + 1] < 4) {
             let neighbour = [posx, posy + 1];
             this.validNeighbours.push(neighbour);
         }
--- docs/js/simulation/citys.mjs
+++ docs/js/simulation/citys.mjs
@@ -4,18 +4,26 @@ export class city {
     constructor(game, x, y, name, size) {
         this.validnextdistricts = []; 
         this.districts = [];
+        this.x = x;
+        this.y = y;
         this.citycenter = this.addDistrict(0, this, x, y, game);
         this.name = name;
         this.size = size;
         for (let i = 0; i < size; i++) {
-            let randomIndex = Math.floor(Math.random() * this.validnextdistricts.length);
-            let [districtX, districtY] = this.validnextdistricts[randomIndex];
-            this.addDistrict(this.districts.length, this, districtX, districtY, game);
+            if(this.validnextdistricts.length>0){
+                let randomIndex = Math.floor(Math.random() * this.validnextdistricts.length);
+                let [districtX, districtY] = this.validnextdistricts[randomIndex];
+                this.addDistrict(this.districts.length, this, districtX, districtY, game);
+            }
         }
+        console.log(this.districts)
+        console.log(this.validnextdistricts)
     }
     addDistrict(id, city, x, y, game) {
         let tempdist = new cityDistrict(id, city, x, y, game);
-        this.validnextdistricts.push(tempdist.validNeighbours);
+        for (let i = 0; i < tempdist.validNeighbours.length; i++) {
+            this.validnextdistricts.push(tempdist.validNeighbours[i]);
+        }
         this.districts.push(tempdist)
     }
     }
