6834ce68b8c934a52032cfd334d089b9b99210d9
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2025-02-26 22:13:59 +0100

    comment

--- docs/js/fileManagement/loadSaveGame.mjs
+++ docs/js/fileManagement/loadSaveGame.mjs
@@ -2,6 +2,7 @@ import { loadJSONFile } from "./loadJSONFile.mjs"
 export async function loadClientSaveGame(game){
     //load the game data from a JSON string
     //pause the start menu scene
+    //TODO: #13 get native neutralino API to work properly
 
     let data = loadJSONFile(game)
     return data
