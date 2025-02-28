b989c66600b1fdb9cd7b9c624a5506cc5114a770
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2025-02-28 20:19:15 +0100

    removed auxiliary directory

--- metro/docs/js/fileManagement/downloadFile.mjs
+++
@@ -1,8 +0,0 @@
-export function downloadFile(path, fileName) {
-    var link = document.createElement("a");
-    link.download = fileName;
-    link.href = path;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  }
--- metro/docs/js/input/keybinds.mjs
+++
@@ -1,6 +0,0 @@
-import { cameraControls } from "./cameraControls.mjs";
-
-//manages all the keyboard keybinds and what they do
-export function keyboardControls(game){
-    cameraControls(game);
-}
